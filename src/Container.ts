763b0781c188b34614063ff99101cd920df49542
Frondor <federico.vazquez@mercadolibre.com>
2021-04-12 18:05:12 -0300

    Add method comments

--- src/Container.ts
+++ src/Container.ts
@@ -7,11 +7,19 @@ export default class Container {
   protected instances = new Map()
   protected aliases = new Map()
 
+  /**
+   * Checks if anything has been bound or aliased into the Container
+   */
   has(key: Key) {
     const alias = this.getAlias(key)
     return this.bindings.has(alias) || this.instances.has(alias)
   }
 
+  /**
+   * Binds a resolvable function into the Container
+   *
+   * If it is the only param, the object reference works as the reference key
+   */
   bind(key: Key | Resolvable<unknown>, resolvable?: unknown | Resolvable<unknown>, shared = false) {
     if (!key) {
       throw new TypeError('First parameter is required')
@@ -25,7 +33,7 @@ export default class Container {
     throw new TypeError(`Only functions are bindable, got [${typeof resolvable}]`)
   }
 
-  resolve(key: Key, params?: unknown[]) {
+  private resolve(key: Key, params?: unknown[]) {
     const alias = this.getAlias(key)
     const instance = this.getInstance(alias)
 
@@ -43,6 +51,12 @@ export default class Container {
     }
   }
 
+  /**
+   * Resolves a Binding reference from the Container
+   *
+   * If the given key is a Resolvable function,
+   * it is resolved from the Container on the fly
+   */
   make(key: unknown, ...params: unknown[]) {
     const binding = this.resolve(key, params)
 
@@ -55,6 +69,9 @@ export default class Container {
     throw new TypeError(`Can not resolve anything for ${key}`)
   }
 
+  /**
+   * Resolves a Binding reference from the Container
+   */
   get<T extends new (...p: any[]) => InstanceType<T>>(key: T): InstanceType<T>
   get<T>(key: T): T extends new () => infer I ? I : unknown
   get(key: unknown): unknown {
@@ -65,23 +82,37 @@ export default class Container {
     throw new TypeError(`${key} is not bound`)
   }
 
+  /**
+   * Set an object or primitive value into the Container
+   *
+   * When resolved, it returns the same value and reference
+   */
   instance(key: Key, value: unknown) {
     this.instances.set(key, value)
   }
 
+  /**
+   * Bind a Resolvable function into the Container.
+   *
+   * The function is resolved once. Its result is stored
+   * and returned on subsequent calls to `get` or `make`.
+   */
   singleton(key: Key, resolvable: unknown) {
     return this.bind(key, resolvable, true)
   }
 
-  getInstance(key: Key) {
+  private getInstance(key: Key) {
     return this.instances.has(key) ? this.instances.get(key) : null
   }
 
+  /**
+   * Apply an alias to an already-bound binding
+   */
   alias(alias: unknown, key: Key | Resolvable<unknown>) {
     this.aliases.set(alias, key)
   }
 
-  getAlias(alias: unknown) {
+  private getAlias(alias: unknown) {
     const key = this.aliases.has(alias) ? this.aliases.get(alias) : null
     return key || alias
   }
